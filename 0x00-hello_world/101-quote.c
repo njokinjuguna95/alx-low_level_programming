babd09cba4169450f07d5c3f3256c00fef88307f
root <njokinjuguna75@gmail.com>
2022-12-10 05:03:19 -0800

    standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,33 +1,14 @@
 # include <stdio.h>
 
 /**
- *
- *  * main - printing a line of code using printf
- *
- *   *
- *
- *    * Return: 1
- *
- *    */
+ * main - printing a line of code using printf
+ * Return description
+ * Return: Always return 0
+ */
 
 int main(void)
-
 {
-
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	printf("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
 
 	return (1);
-
 }
-
-run betty after:
-
-betty 101-quote.c
-
-compile:
-
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -o quote 101-quote.c
-
-run this too :
-
-./quote
