35b3c6cb60f74bcb0dc0f1e605730a6d509ac5f3
root <njokinjuguna75@gmail.com>
2022-12-10 01:49:06 -0800

    puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 # include <stdio.h>
 /**
- *main - printing a line of code using prin
+ *main - printing a line of code using printf
  *
  * Return:0
  *
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0)
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
 
