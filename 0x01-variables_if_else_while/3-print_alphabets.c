f41d6b4526edf7344fa2c22c7e3be751c2283064
root <njokinjuguna75@gmail.com>
2022-12-13 07:46:53 -0800

    lower then upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - lowercase then uppercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char i;
+	i = 'a';
+	for (i = 'a'; i <= 'z'; i++)
+		{
+		putchar(i);
+		}
+	for (i = 'A'; i < 'Z'; i++)
+		{
+		putchar(i);
+		}
+	putchar('\n');
+	return (0);
+}
