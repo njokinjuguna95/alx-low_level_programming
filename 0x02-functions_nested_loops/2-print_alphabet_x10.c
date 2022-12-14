f5493d0d271df85658ef3e7af485d098651804aa
root <njokinjuguna75@gmail.com>
2022-12-14 05:10:55 -0800

    10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,14 @@
  * Return: Always return 0
  */
 
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
 		int count = 0;
 		char x;
 
 		while (count++ <= 9)
 		{
-			for (x = 'a'; x<= 'z'; x++)
+			for (x = 'a'; x <= 'z'; x++)
 				_putchar(x);
 
 			_putchar('\n');
