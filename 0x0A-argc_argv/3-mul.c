85fa64cd43030a1f813205d779a5b8f7bc699808
njokinjuguna95 <njokinjuguna75@gmail.com>
2023-01-12 13:54:06 +0000

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - multiplies two numbers and prints result
@@ -11,9 +12,15 @@ int main(int argc, char **argv)
 {
 	int a, b;
 	
-	printf("Enter two numbers\n");
-	scanf("%d %d", &a, &b);
-	printf("Multiplication of %d and %d is %d\n", a, b, (a*b));
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
 
+	printf("%d\n", a*b);
+		
 	return (0);
 }
